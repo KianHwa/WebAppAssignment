064805c09b4a3b14cf916585a06da85b843754bc
So Kian Hwa <sokianhwa9-9@outlook.com>
2019-07-02 22:59:12 +0800

    a

--- SQLQuery1.sql
+++
@@ -1,3 +0,0 @@
-﻿Select * from aspnet_Membership inner join aspnet_Users 
-on aspnet_Membership.UserId = aspnet_Users.UserId
-inner join Artwork on aspnet_Membership.UserId = Artwork.artistID 
--- SQLQuery2.sql
+++
@@ -1,8 +0,0 @@
-﻿select Artwork.artworkName from 
-Member inner join Orders
-on member.memberID = orders.memberID
-inner join OrderDetails 
-on Orders.orderID = OrderDetails.orderID
-inner join Artwork 
-on OrderDetails.artworkID = Artwork.artworkID
-where Member.memberName = 'Ho'
